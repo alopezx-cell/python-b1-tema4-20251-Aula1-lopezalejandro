cf6c7c0c50de7d5787d5213f176fac56fdf93a06
alopezboix <alopezboix@gmail.com>
2026-06-15 18:21:47 +0200

    ej4d1 suma con input

--- 4d/ej4d1.py
+++ 4d/ej4d1.py
@@ -35,8 +35,11 @@ Exemple:
 """
 
 def sum():
-    # Write here your code
-    pass
+    n1 = int(input("Insert the first number: "))
+    n2 = int(input("Insert the second number: "))
+    resultado = n1 + n2
+    print("Result:", resultado)
+    return resultado
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
